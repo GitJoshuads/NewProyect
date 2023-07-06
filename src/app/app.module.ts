49aebf41a9d8259f40b139f1f98abefa2833b3f5
GitJoshuads <garciaperezjose852@gmail.com>
2023-07-06 18:31:55 +0200

    eliminar prueba

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MenuComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [MenuComponent]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
