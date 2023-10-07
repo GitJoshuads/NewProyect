5c267d90ad86c2e53b313a80fec251b5926aee74
Joshua <garciaperezjose852@gmail.com>
2023-10-07 22:41:55 +0200

    mostrar simbolo y precio

--- src/app/components/llamada-binance/llamada-binance.component.ts
+++ src/app/components/llamada-binance/llamada-binance.component.ts
@@ -1,58 +1,25 @@
-import { Component, Input, inject } from '@angular/core'; 
-import {HttpClient} from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { CryptoPriceService } from '../../crypto-price.service';
 
 
-@Component({
-  selector: 'app-llamada-binance',
-  templateUrl: './llamada-binance.component.html',
-  styleUrls: ['./llamada-binance.component.scss']
-})
-export class LlamadaBinanceComponent {
-  @Input() endpoint: string = '';
-  @Input() data: Array<string> = [];
-constructor() {
-  //let data = [];
-  let endpoint = {
-    type: String,
-  } 
-  let mostrarData = "";
-}
-
-http = inject(HttpClient);
-
-ngOnInit(){
-  this.http.get('https://api.binance.com/api/v3/ticker/price')
-  .subscribe((data: any)=>{
-    console.log(data);
-    this.mostrarData(data);
+  
+  @Component({
+    selector: 'app-llamada-binance',
+    templateUrl: './llamada-binance.component.html',
+    styleUrls: ['./llamada-binance.component.scss']
   })
+  export class LlamadaBinanceComponent implements OnInit {
+    cryptoData: any[] = [];
   
-}
-mostrarData = (data: any)=>{
-  console.log(data + "data antes de entrar al for")
-  let body = ''
-  for (let i=0; i < data.length; i++) {
-      body += `<tr><td>${data[i].symbol}</td><td>${data[i].price}</td></tr>`
+    constructor(private cryptoPriceService: CryptoPriceService) {}
+  
+    ngOnInit(): void {
+      this.getCryptoPrices();
+    }
+  
+    getCryptoPrices(): void {
+      this.cryptoPriceService.getCryptoPrices().subscribe((data) => {
+        this.cryptoData = data;
+      });
+    }
   }
-
-  //document.getElementById('data').innerHTML = data;
-  
-} 
-
-/*
-LlamadaBinanceComponent.fetch(endpoint)
-      .then( respuesta => respuesta.json() )
-      .then( datos => mostrarData(datos))
-      .catch( e => console.log(e))
-  
-  
-  const mostrarData = (data)=>{
-      //console.log(data)
-      let body = ''
-      for (let i=0; i < data.length; i++) {
-          body += `<tr><td>${data[i].symbol}</td><td>${data[i].price}</td></tr>`
-      }
-      document.getElementById('data').innerHTML = body
-  } 
-  */   
-}
