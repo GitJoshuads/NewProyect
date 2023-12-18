b4a6ec52b5f3b555a1a30d94df73f22dd7ee213c
Joshua <garciaperezjose852@gmail.com>
2023-12-18 21:36:29 +0100

    colores grafica

--- src/app/components/donut-chart/donut-chart.component.ts
+++ src/app/components/donut-chart/donut-chart.component.ts
@@ -23,11 +23,17 @@ export class DonutChartComponent {
     name: 'custom',
     selectable: true,
     group: ScaleType.Ordinal,
-    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
+    domain: ["#1f78b4", "#33a02c", "#e31a1c", "#ff7f00", "#6a3d9a",
+    "#a6cee3", "#b2df8a", "#fb9a99", "#fdbf6f", "#cab2d6",
+    "#01665e", "#8c510a", "#35978f", "#c51b7d", "#b2182b",
+    "#084594", "#41ab5d", "#67001f", "#8c6bb1", "#8e0152",
+    "#3182bd", "#006837", "#fa9fb5", "#fec44f", "#d73027",
+    "#084594", "#8856a7", "#41ab5d", "#d73027", "#313695",
+    "#fdae61", "#fee08b", "#d73027", "#4575b4", "#313695",
+    "#fee08b", "#4575b4", "#313695", "#fee08b", "#4575b4",
+    "#313695", "#fee08b", "#4575b4", "#313695", "#fee08b",
+    "#4575b4", "#313695", "#fee08b", "#4575b4", "#313695"]
   };
-  //colorScheme: { domain: string[] } = { domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA'] };
-  //colorScheme: string[] = ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA'];
-  //colorScheme = {domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']};
 
   constructor() {
    
